9ab37591e2ffeb3e1d0f0e6f7e9bdbd10e47cd2d
Murilo <mufe.cremonesi@gmail.com>
2026-06-04 15:22:19 -0300

    add file sql

---
+++ banco.sql
@@ -0,0 +1,43 @@
+create database treefolio;
+use treefolio;
+
+create table usuario (
+id_user int primary key auto_increment,
+nome varchar(50),
+email varchar(50) unique,
+fone  varchar(20) unique,
+status enum('ativo', 'inativo') default 'ativo',
+foto varchar(255)
+);
+
+create table projetos (
+id_projeto int primary key auto_increment,
+id_user int,
+titulo varchar(50) not null,
+descricao text,
+dataproj datetime default current_timestamp,
+
+foreign key (id_user) references usuario(id_user)
+);
+
+create table post (
+id_post int primary key auto_increment,
+id_user int,
+id_projeto int null,
+file varchar(255),
+legenda text,
+datapost datetime default current_timestamp,
+likes int default 0,
+feed boolean default true,
+
+foreign key(id_user) references usuario(id_user),
+foreign key (id_projeto) references projetos(id_projeto)
+);
+
+alter table usuario
+add bio text;
+
+select * from usuario;
+
+
+
