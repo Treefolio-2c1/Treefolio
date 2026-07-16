68713e0b873198b38d4b7edc19c25af5bdd4ca2c
Murilo <duplabuarque@gmail.com>
2026-07-16 20:12:22 -0300

    botao de logout, logowhite.svg

--- index.php
+++ index.php
@@ -1,20 +1,35 @@
+<?php 
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR" data-theme="light">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Static/Styles/style.css">
   <title>Treefolio — Gerencie seu portfólio com inteligência</title>
 </head>
 <body>
 
 <h1>Index</h1>
 
-<a href="auth/cadastro.php">
-    <button>Cadastro</button>
-</a>
-<a href="auth/login.php">
-    <button>login</button>
-</a>
+<button onclick="window.location.href='auth/cadastro.php'">
+    Cadastro
+</button>
+
+<button onclick="window.location.href='auth/login.php'">
+    Login
+</button>
+
+<button onclick="window.location.href='auth/logout.php'">
+    Logout
+</button>
 
 
 
