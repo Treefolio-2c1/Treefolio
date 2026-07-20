054bd8110f42803ef56359bebaa5e36ced17995c
Murilo <duplabuarque@gmail.com>
2026-07-20 17:59:07 -0300

    add include.css

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,9 @@
-<style>
-footer {
-    width: 100%;
-    padding: 10px 0;
-    margin-top: auto;
-    text-align: center;  
-    background-color: #121314;
-    color: white;
-    font-size: 12px;
-}
-
-.footer p {
-    margin: 3px;
-}
-</style>
+<link rel="stylesheet" href="Static/Styles/include.css">
 
 <footer class="footer">
 
     <p>
         2026 Treefolio. 
     </p>
-
-
+    
 </footer>
---
+++ includes/header.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="Static/Styles/include.css">
